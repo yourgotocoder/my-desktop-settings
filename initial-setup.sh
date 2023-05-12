07f818505b3a482bdbf8c86c6f130c98766f8eed
Sudarshan Rai <yourgotocoder@gmail.com>
2023-05-12 16:12:46 +0530

    Initial software download script

---
+++ initial-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Update the local softwares
+sudo apt-get -y update && sudo apt-get -y upgrade
+#Install gcc, g++ and curl
+sudo apt-get install -y build-essential curl
+#Install nvm for node version management
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+source ~/.profile
+#Install node
+nvm install v18.16.0
+#Install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+#Install neovim
+sudo snap install -y nvim
