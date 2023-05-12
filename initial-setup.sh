453677b3a28f0cbeec27169839ba6eb11d651dfb
Sudarshan Rai <yourgotocoder@gmail.com>
2023-05-12 16:25:58 +0530

    Using sequential installation

--- initial-setup.sh
+++ initial-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Update the local softwares
 sudo apt-get -y update && sudo apt-get -y upgrade
-#Install gcc, g++ and curl
-sudo apt-get install -y build-essential curl
+#Install gcc, g++ curl, git
+sudo apt-get install -y build-essential curl git
 #Install nvm for node version management
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 source ~/.profile
@@ -12,3 +12,5 @@ nvm install v18.16.0
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 #Install neovim
 sudo snap install -y nvim
+sudo apt-get install -y zsh
+
